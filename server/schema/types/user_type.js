b35ce9911a8a9893e8cb887837177425876172e2
Michael Yiu <onew1ng3d@gmail.com>
2018-11-18 22:45:10 -0500

    Apollo provider(glue to react) added to client

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -1,13 +1,15 @@
 const graphql = require('graphql');
 const {
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLID
 } = graphql;
 
 // no instance where we need to include users passwords. it should never be shared or public
 const UserType = new GraphQLObjectType({
   name: 'UserType',
   fields: {
+    id: { type: GraphQLID }, //to activate the property in index.js
     email: { type: GraphQLString }
   }
 });
